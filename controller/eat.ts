0a605f5c95c40504a935dc733bc61314edbe7bd7
Y.Jung <gilsang9808@gmail.com>
2019-11-18 17:57:07 +0900

    급식 사이트 만들기

---
+++ controller/eat.ts
@@ -0,0 +1,7 @@
+
+export default (date: string, time: string) => {
+    // 월요일~금요일을 18~22로 바꾸기
+    // 조식/중식/석식을 0 ~ 2로 바꾸기
+    // model에서 data 불러오기
+    return "급식";
+}
--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 var router = express.Router();
-import sum from '../controller/number';
+import eat from '../controller/eat';
 
-router.get('/to', function(req, res, next) {
-  res.send({"to":sum(req.query.num)});
+router.get('/', function(req, res, next) {
+  res.send({"to":eat(req.query.date, req.query.time)});
 });
 
 export default router;
