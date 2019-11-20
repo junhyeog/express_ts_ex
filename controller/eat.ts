2d95fd42c3975212c9cbce2aedd83cf6f978604b
junhyeog <junhyeog@korea.ac.kr>
2019-11-21 01:59:56 +0900

    semantic url ex

--- controller/eat.ts
+++ controller/eat.ts
@@ -6,14 +6,16 @@ export default (date: string, time: string) => {
 	// 조식/중식/석식을 0 ~ 2로 바꾸기
 	// model에서 data 불러오기
 
-	var dayNum = 18
-	if (date == "tus") dayNum = 19
+	var dayNum = -1
+	if (date == "mon") dayNum = 18
+	else if (date == "tue") dayNum = 19
 	else if (date == "wen") dayNum = 20
-	else if (date == "tur") dayNum = 21
+	else if (date == "thu") dayNum = 21
 	else if (date == "fri") dayNum = 22
 
-	var timeNum = 0
-	if (time == "lunch") timeNum = 1
+	var timeNum = -1
+	if (time == "morning") timeNum = 0
+	else if (time == "lunch") timeNum = 1
 	else if (time == "dinner") timeNum = 2
 	const menus = recipe[dayNum - 18][timeNum]
 	console.log(recipe[dayNum - 18][timeNum])
--- routes/index.ts
+++ routes/index.ts
@@ -3,8 +3,19 @@ var router = express.Router()
 import eat from "../controller/eat"
 
 router.get("/", function(req, res, next) {
-	res.render("index", eat(req.query.date, req.query.time))
-	// res.send(eat(req.query.date, req.query.time))
+	const dateList = { mon: 0, tue: 0, wen: 0, thu: 0, fri: 0 }
+	const timeList = { morning: 0, lunch: 0, dinner: 0 }
+	const date = req.query.date
+	const time = req.query.time
+	if (!(date in dateList) || !(time in timeList)) res.send("Bad Request!")
+	else res.render("index", eat(date, time))
 })
-
+router.get("/semantic/:date/:time", function(req, res, next) {
+	const dateList = { mon: 0, tue: 0, wen: 0, thu: 0, fri: 0 }
+	const timeList = { morning: 0, lunch: 0, dinner: 0 }
+	const date = req.params.date
+	const time = req.params.time
+	if (!(date in dateList) || !(time in timeList)) res.send("Bad Request!")
+	else res.render("index", eat(date, time))
+})
 export default router
