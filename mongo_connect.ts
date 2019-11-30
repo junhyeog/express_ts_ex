2f2408446c654de437cc80bdcff49aaa3d6552ea
junhyeog <junhyeog@korea.ac.kr>
2019-11-30 22:26:52 +0900

    local db 사용법 추가

--- mongo_connect.ts
+++ mongo_connect.ts
@@ -14,3 +14,11 @@ export default function mongo_connect() {
 			await setTimeout(mongo_connect, 3000)
 		})
 }
+
+/*
+C:\Program Files\MongoDB\Server\4.2\bin> mongo
+> use diet
+> db.printCollectionStats() # diets 찍힘
+> db.diets.help() # 모를 떈 .help() 붙이면 도움말 나옴 -> find 함수 확인
+> db.diets.find() # data 확인
+*/
